5d39fe3d3d942d4e66b877f5e14258748d86c7a3
alejo <alejo@ALEJO>
2024-09-23 20:42:41 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1-2024C2-12C-BasicoConsola/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_2024C2_12C_BasicoConsola
+{
+    internal class Persona
+    {
+        public String dni { get; set; }
+        public String apellido { get; set; }
+        public int edad { get; set; }
+
+        public Persona(string dni, string apellido, int edad)
+        {
+            this.dni = dni;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+
+        public string mostrarDatos()
+        {
+            return "El dni de la persona es " + this.dni + ", El apellido es " + this.apellido + ", y la edad es de " + this.edad;
+        }
+    }
+}
---
+++ ConsoleApp1-2024C2-12C-BasicoConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1_2024C2_12C_BasicoConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");             
+
+            Persona p = new Persona("3232", "Perez", 35);
+
+            Console.WriteLine(p.mostrarDatos());
+
+        }
+    }
+}
